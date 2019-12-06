0826354f6fe7b0a3b283c788132dc2c277dbc045
Ryan Schuetzler <ryan@schuetzler.net>
2019-12-06 15:54:12 -0600

    Day 4, part 2

--- src/day4/day4.py
+++ src/day4/day4.py
@@ -2,13 +2,18 @@ import re
 
 
 def check_password(password):
-    repeats_two = re.search(r"(\d)\1", str(password))
-    repeats_three = re.search(r"(\d)\1\1", str(password))
+    repeats = re.findall(r"((\d)\2+)", str(password))
+    repeats_two = False
+    for repeat in repeats:
+        # print(repeat)
+        if len(repeat[0]) == 2:
+            repeats_two = True
+
     digits = [int(char) for char in str(password)]
     increasing = (
         digits[0] <= digits[1] <= digits[2] <= digits[3] <= digits[4] <= digits[5]
     )
-    return repeats_two and increasing and not repeats_three
+    return repeats_two and increasing
 
 
 def enumerate_passwords(start, end):
@@ -19,8 +24,9 @@ def enumerate_passwords(start, end):
 if __name__ == "__main__":
     START = 240298
     END = 784956
+    # START = 123456
+    # END = 223344
     matching_passwords = enumerate_passwords(START, END)
     print(matching_passwords)
     print(len(matching_passwords))
-    
 
